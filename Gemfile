1c4e5452b90e34234f5a15c1738e31aeed14854b
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2019-09-19 08:51:58 +0100

    add-bullet-and-process-mem-gems

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.1.7'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'bullet'
+gem 'get_process_mem'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
+require 'get_process_mem'
+
 class ApplicationController < ActionController::Base
+  before_action :print_before_memory
+  after_action :print_after_memory
   protect_from_forgery with: :exception
+
+  def print_usage(description)
+    mb = GetProcessMem.new.mb
+    puts "#{ description } - MEMORY USAGE(MB): #{ mb.round }"
+  end
+
+  def print_before_memory
+    print_usage("Before")
+  end
+
+  def print_after_memory
+    print_usage("After")
+  end
 end
