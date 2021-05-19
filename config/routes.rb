fb2666e841034156127c50c81207fd6c39dde96e
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2021-05-19 12:54:06 +0100

    multiple records create

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  post '/bulk_create_articles', to: 'articles#bulk_create_articles'
   root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
